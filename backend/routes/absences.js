70cc7209efe929e4e6ffcf5bc4b97061630aba3e
itc-s21009 <s21009@std.it-college.ac.jp>
2023-02-17 23:47:08 +0900

    /absencesの表示形式を直した

--- backend/routes/absences.js
+++ backend/routes/absences.js
@@ -2,14 +2,36 @@ const express = require('express')
 const router = express.Router()
 const prisma = require('../index')
 
+const KIND_NAMES = {
+    1: '欠席',
+    2: '就活欠席',
+    3: '遅刻',
+    4: '早退',
+}
+
 router.get('/', async (req, res) => {
     const student = res.locals.student
     const absences = await prisma.absence.findMany({
         where: {
             student_id: student.id
-        }
+        },
+        include: {
+            student: true
+        },
     })
-    res.json(absences)
+    const data = absences.map((a) => (
+        {
+            id: a.id,
+            kind: KIND_NAMES[a.kind],
+            reason: a.reason,
+            start_date: a.start_date,
+            end_date: a.end_date,
+            created_at: a.created_at,
+            student_code: a.student.code,
+            student_name: a.student.name,
+        }
+    ))
+    return res.json(data)
 })
 
 router.post('/add', async (req, res) => {
