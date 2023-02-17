cc4f4ab6ec00bbd1a04e98728801ab067c30ea7f
itc-s21009 <s21009@std.it-college.ac.jp>
2023-02-18 00:59:14 +0900

    /certificateで証明書発行申請履歴の取得まで

--- backend/routes/certificate.js
+++ backend/routes/certificate.js
@@ -12,6 +12,60 @@ const PRICES = {
     health_certificate: 100
 }
 
+/**
+ * 証明書IDとその枚数のデータから、合計枚数と合計金額を返します
+ * @param detail 証明書IDとその枚数のデータ
+ * @returns {{total_copy: number, total_amount: number}}
+ */
+const calculate = (detail) => {
+    // 足し算する関数
+    const sum = (x, y) => {
+        // undefinedの対処
+        if (!x) x = 0
+        if (!y) y = 0
+        return x + y
+    }
+    // detailのvalue（枚数）だけを取って、reduceで合算する
+    const total_copy = Object.values(detail).reduce(sum)
+    // detailのkey（証明書の種類）をcertTypeに入れて
+    // 各証明書の枚数と費用を計算する
+    const total_amount = Object.keys(detail)
+        .map(certType => {
+            const copies = detail[certType]
+            // undefinedの対処
+            if (!copies) return 0
+            return copies * PRICES[certType]
+        })
+        .reduce(sum)
+    return {
+        total_copy: total_copy,
+        total_amount: total_amount
+    }
+}
+
+router.get('/', async (req, res) => {
+    const student = res.locals.student
+    const applications = await prisma.certificateApplication.findMany({
+        where: {
+            student_id: student.id
+        },
+    })
+    const data = applications.map((application) => {
+        // CertificateApplicationテーブルのデータから、証明書の枚数のデータだけをdetailに入れる
+        const {id, student_id, created_at, scheduled_date, ...detail} = application
+        const {total_copy, total_amount} = calculate(detail)
+        return {
+            id,
+            ...detail,
+            created_at: created_at,
+            scheduled_date: scheduled_date,
+            total_copy: total_copy,
+            total_amount: total_amount
+        }
+    })
+    return res.json(data)
+})
+
 router.post('/apply', async (req, res) => {
     const student = res.locals.student
     const {
@@ -38,25 +92,7 @@ router.post('/apply', async (req, res) => {
             ...applyDetail
         }
     }).then((result) => {
-        // 足し算する関数
-        const sum = (x, y) => {
-            // undefinedの対処
-            if (!x) x = 0
-            if (!y) y = 0
-            return x + y
-        }
-        // applyDetailのvalue（枚数）だけを取って、reduceで合算する
-        const total_copy = Object.values(applyDetail).reduce(sum)
-        // applyDetailのkey（証明書の種類）をcertTypeに入れて
-        // 各証明書の枚数と費用を計算する
-        const total_amount = Object.keys(applyDetail)
-            .map(certType => {
-                const copies = applyDetail[certType]
-                // undefinedの対処
-                if (!copies) return 0
-                return copies * PRICES[certType]
-            })
-            .reduce(sum)
+        const {total_copy, total_amount} = calculate(applyDetail)
         return res.json({
             status: 0,
             receipt: applyDetail,
